8c2f38f11f86ba9d1f0d7c1fa47691cc58cb7330
trungleminh111 <trunglm@students.hueic.edu.vn>
2023-07-14 20:32:46 +0700

    Login/Logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,17 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../assets/logo192.png'
-import { useLocation, NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify';
+
 
 const Header = (props) => {
-    const location = useLocation()
+    const navigate = useNavigate()
+    const handleLogout = () => {
+        localStorage.removeItem("token")
+        navigate("/")
+        toast.success("logout success!")
+    }
     return (<>
         <Navbar expand="lg" className="bg-body-tertiary bg-light">
             <Container>
@@ -28,8 +35,12 @@ const Header = (props) => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Setting" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-                            <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                            <NavDropdown.Item >
+                                <NavLink to="/login" className="nav-link">Login</NavLink>
+                            </NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => handleLogout()}>
+                                Logout
+                            </NavDropdown.Item>
 
                         </NavDropdown>
                     </Nav>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,48 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { loginApi } from '../services/UserService'
 import './Login.scss'
+
+
 const Login = () => {
+    const navigate = useNavigate()
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [isShowPassword, setIsShowPassword] = useState(false)
+    const [loadingAPI, setLoadingAPI] = useState(false)
+    useEffect(() => {
+        let token = localStorage.getItem("token")
+        if (token) {
+            navigate("/")
+        }
+    }, [])
+
+    const handleLogin = async () => {
+        if (!email || !password) {
+            toast.error("Email/Password is required")
+            return;
+        }
+        setLoadingAPI(true)
+        let res = await loginApi(email, password)
+        if (res && res.token) {
+            localStorage.setItem("token", res.token)
+            navigate("/")
+        }
+        else {
+            if (res && res.status === 400) {
+                toast.error(res.data.error)
+
+            }
+        }
+        setLoadingAPI(false)
+        console.log(res);
+    }
 
     return (<>
         <div className="login-container col-12 col-sm-4">
             <div className="title">Login</div>
-            <div className="text">Email or Username</div>
+            <div className="text">Email or Username(eve.holt@reqres.in)</div>
             <input type="text"
                 placeholder='Email or Username'
                 value={email}
@@ -20,9 +54,12 @@ const Login = () => {
                     value={password}
                     onChange={(event) => setPassword(event.target.value)}
                 />
-                <i onClick={() => setIsShowPassword(!isShowPassword)} class={`fa-solid ${isShowPassword ? 'fa-eye' : 'fa-eye-slash'}`}></i>
+                <i onClick={() => setIsShowPassword(!isShowPassword)} className={`fa-solid ${isShowPassword ? 'fa-eye' : 'fa-eye-slash'}`}></i>
             </div>
-            <button className={email && password ? 'active' : ''} disabled={email && password ? false : true}>Login</button>
+            <button className={email && password ? 'active' : ''}
+                disabled={email && password ? false : true}
+                onClick={() => handleLogin()}
+            > {loadingAPI && <i className='fa-solid fa-sync fa-spin'></i>} &nbsp; Login</button>
             <div className='back'>
                 Go back
             </div>
